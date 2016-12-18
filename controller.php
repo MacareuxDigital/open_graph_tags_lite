6e7ca94fc65aff62dc68a9a23d617e90a59a0f0e
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2016-12-19 05:23:31 +0900

    Increase package version

--- controller.php
+++ controller.php
@@ -12,7 +12,7 @@ class Controller extends \Concrete\Core\Package\Package {
 
     protected $pkgHandle = 'open_graph_tags_lite';
     protected $appVersionRequired = '5.7.4';
-    protected $pkgVersion = '2.1.2';
+    protected $pkgVersion = '2.1.3';
     
     public function getPackageDescription()
     {
